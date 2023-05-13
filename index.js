837741395b86790e0e4741d8ec1da8881378fc37
Silte1 <etiennesilue3@gmail.com>
2023-05-13 22:10:22 +0200

    correct the reset button

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ resetButton = document.getElementById('button-reset'),
 hour = document.getElementById('hours'),
 minute = document.getElementById('minutes'),
 second = document.getElementById('seconds'),
-ten = document.getElementById('tens');
+millisecond = document.getElementById('millisecond');
 
 let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
 int = null
@@ -25,8 +25,14 @@ wrapper.addEventListener('click', (e)=>{
         clearInterval(int)
     } else if(e.target.id === 'button-reset'){
         clearInterval(int)
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+        milliseconds = 0;
+        minute.innerHTML = '00';
+        hour.innerHTML = '00';
         second.innerHTML = '00';
-        ten.innerHTML = '00';
+        millisecond.innerHTML = '00';
         startButton.innerText = 'Start'
 
     }
@@ -53,58 +59,8 @@ timeDisplay = () =>{
     hour.innerHTML = h
     minute.innerHTML = m
     second.innerHTML = s;
-    ten.innerHTML = ms;
-
-  
+    millisecond.innerHTML = ms;
 }
 
 
 
-/*let [milliseconds,seconds,minutes,hours] = [0,0,0,0];
-let timerRef = document.querySelector('#timerRef');
-let int = null;
-let second = document.getElementById('seconds');
-let ten = document.getElementById('tens');
-
-document.getElementById('button-start').addEventListener('click', ()=>{
-    if(int!==null){
-        clearInterval(int);
-    }
-    int = setInterval(displayTimer,10);
-});
-
-document.getElementById('button-stop').addEventListener('click', ()=>{
-    clearInterval(int);
-});
-
-document.getElementById('button-reset').addEventListener('click', ()=>{
-    clearInterval(int);
-    // [milliseconds,seconds,minutes,hours] = [0,0,0,0];
-    second.innerHTML = 00;
-    ten.innerHTML = 00;
-
-});
-
-function displayTimer(){
-    milliseconds+=10;
-    if(milliseconds == 1000){
-        milliseconds = 0;
-        seconds++;
-        if(seconds == 60){
-            seconds = 0;
-            minutes++;
-            if(minutes == 60){
-                minutes = 0;
-                hours++;
-            }
-        }
-    }
-
- let h = hours < 10 ? "0" + hours : hours;
- let m = minutes < 10 ? "0" + minutes : minutes;
- let s = seconds < 10 ? "0" + seconds : seconds;
- let ms = milliseconds < 10 ? "00" + milliseconds : milliseconds < 100 ? "0" + milliseconds : milliseconds;
-
- second.innerHTML = s;
- ten.innerHTML = ms;
-}*/
